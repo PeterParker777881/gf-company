736a17192b92a5dfaf82a03820df26c741f7b4af
Madi <piterparker777881@gmail.com>
2023-02-27 23:40:39 +0600

    first commit

---
+++ src/components/app/Header.vue
@@ -0,0 +1,106 @@
+<template>
+  <header class="header">
+    <div class="header-top">
+      <div class="container">
+        <div class="header-top-inner">
+          <img class="header-top-logo" src="@/assets/images/header/header-logo.png" />
+          <nav class="header-top-nav">
+            <ul>
+              <li class="header-top-nav-li" v-for="(link, i) in headerTopList" :key="i">
+                <router-link class="header-top-nav-link" :to="link.href">{{ link.title }}</router-link>
+              </li>
+            </ul>
+          </nav>
+          <Search class="search" />
+          <div class="header-top-right">
+            <router-link class="header-top-right-link" to="#">
+              <img src="@/assets/images/header/cart.svg" />
+            </router-link>
+            <router-link class="header-top-right-link" to="#">
+              <img src="@/assets/images/header/profile.svg" />
+              <span>Личный кабинет</span>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import Search from '@/components/app/Search.vue'
+export default {
+  components: { Search },
+  data: () => ({
+    headerTopList: [
+      { title: 'Интересности и полезности', href: '#' },
+      { title: 'Контакты', href: '#' },
+      { title: 'Отзывы', href: '#' },
+      { title: 'Доставка и оплата', href: '#' }
+    ]
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+.header-top {
+  background: #f8f8f8;
+  padding: 12px 0;
+}
+.header-top-inner {
+  display: flex;
+  align-items: center;
+}
+.header-top-logo {
+  display: block;
+  width: 100%;
+  max-width: 73px;
+  height: 60px;
+  margin-right: 28px;
+}
+.header-top-nav {
+  margin-right: 123px;
+  ul {
+    display: flex;
+  }
+}
+.header-top-nav-li {
+  margin-right: 24px;
+  &:last-child {
+    margin-right: 0;
+  }
+}
+.header-top-nav-link {
+  line-height: 22px;
+  font-weight: 500;
+  color: #000;
+  text-decoration: none;
+}
+.search {
+  margin-right: 32px;
+}
+.header-top-right {
+  display: flex;
+  align-items: center;
+}
+.header-top-right-link {
+  display: flex;
+  align-items: center;
+  margin-right: 32px;
+  text-decoration: none;
+  img {
+    width: 26px;
+    height: 26px;
+  }
+  span {
+    display: block;
+    margin-left: 8px;
+    line-height: 22px;
+    font-weight: 500;
+    color: #000;
+  }
+  &:last-child {
+    margin-right: 0;
+  }
+}
+</style>
---
+++ src/components/app/Search.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="search">
+    <img class="glass" src="@/assets/images/search/glass.svg">
+    <input class="input" type="text" label="Поиск">
+    <img class="cancel" src="@/assets/images/search/cancel.svg">
+  </div>
+</template>
+
+<style lang="scss">
+.search {
+  padding: 13px 12px;
+  background: #fff;
+  border-radius: 8px;
+  position: relative;
+  width: 288px;
+  height: 50px;
+}
+.glass {
+  width: 24px;
+  height: 24px;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  left: 12px;
+}
+.input {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  padding: 0 48px;
+  background: transparent;
+  border: none;
+  z-index: 1;
+}
+.cancel {
+  width: 12px;
+  height: 12px;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  right: 19px;
+  cursor: pointer;
+  z-index: 2;
+}
+</style>
